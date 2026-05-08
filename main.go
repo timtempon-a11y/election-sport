20fd28c22d3d437aa107580dff95a731717e2e80
timtempon-a11y <timtempon@gmail.com>
2026-05-08 21:40:58 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+)
+
+func vote(w http.ResponseWriter, r *http.Request) {
+	/*
+		Lecture données envoyées
+	*/
+	body, _ := io.ReadAll(r.Body)
+	/*
+		Anciennes données
+	*/
+	var data []map[string]any
+
+	file, err := os.ReadFile("data.json")
+
+	if err == nil {
+
+		json.Unmarshal(file, &data)
+	}
+
+	/*
+		Nouvelle donnée
+	*/
+
+	var newData map[string]interface{}
+
+	json.Unmarshal(body, &newData)
+
+	/*
+		Ajout
+	*/
+
+	data = append(data, newData)
+
+	/*
+		Sauvegarde
+	*/
+
+	finalData, _ := json.MarshalIndent(
+		data,
+		"",
+		"  ",
+	)
+
+	os.WriteFile(
+		"data.json",
+		finalData,
+		0644,
+	)
+
+	/*
+		Réponse
+	*/
+
+	w.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+
+	w.Write([]byte(`{
+		"success":true,
+		"message":"Vote reçu"
+	}`))
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func FolderPublic(w http.ResponseWriter, r *http.Request) {
+	http.StripPrefix(
+		"/Public/",
+		http.FileServer(http.Dir("./Public")),
+	).ServeHTTP(w, r)
+
+}
+
+func main() {
+
+	http.HandleFunc("/home",Home)
+	http.HandleFunc("/Public",FolderPublic)
+
+	
+	http.HandleFunc(
+		"/vote",
+		vote,
+	)
+
+	http.ListenAndServe(
+		":8080",
+		nil,
+	)
+}
